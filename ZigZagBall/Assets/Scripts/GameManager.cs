ea983d3fedb8387e93c1c6a39373006fab4be9f4
Daniel Stankovic <danielstankovic1992@gmail.com>
2017-11-30 21:37:03 +0100

    The ZigZagBall game
    A simple game to kill time.

---
+++ ZigZagBall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public bool gameOver; 
+	public static GameManager instance; 
+
+	void Awake(){
+
+		if(instance == null){
+			instance = this; 
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 public void GameStart(){
+		UiManager.instance.GameStart ();
+		ScoreManager.instance.startScore ();
+		GameObject.Find ("PlatformSpawner").GetComponent<PlatformSpawner> ().PlatformSpawn ();
+
+	}
+
+	public void GameOver(){
+		UiManager.instance.GameOver ();
+		ScoreManager.instance.stopScore ();
+		gameOver = true; 
+
+	}
+}
